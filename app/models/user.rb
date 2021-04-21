04eaf820e17c569cf662c2c68d392bbacd9a829c
hiroki1121 <81612317+hiroki1121@users.noreply.github.com>
2021-04-21 13:24:59 +0900

    新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :email, uniqueness: { case_sensitive: true }
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_ruby, presence: true
+  validates :first_name_ruby, presence: true
+  validates :birthday, presence: true
 end
